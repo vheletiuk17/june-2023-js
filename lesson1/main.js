a7fa46a41960be446e9b8e0f60f40a21a3d9fad2
vheletiuk17 <vheletiuk17@gmail.com>
2023-08-16 19:16:41 +0300

    hw

---
+++ lesson1/main.js
@@ -0,0 +1,64 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let a =`hello`
+console.log(a);
+
+let b =`owu`
+console.log(b);
+
+let c =`com`
+console.log(c);
+
+let d =`ua`
+console.log(d);
+
+let num =1
+console.log(num);
+
+let num1 =10
+console.log(num1);
+
+let num2 =-999
+console.log(num2);
+
+let PI =3.14
+console.log(PI);
+
+let num3 =2.7
+console.log(num3);
+
+let num4 =16
+console.log(num4);
+
+let k =true
+console.log(k);
+
+let n =false
+console.log(n);
+
+
+let firstName =`Heletiuk`
+console.log(firstName);
+let middleName =`Vasyl`
+console.log(middleName);
+let lastName =`Vasilovich`
+console.log(lastName);
+let results =`${firstName} ${middleName} ${lastName}`
+console.log(results)
+
+let a1 = 100;
+console.log(typeof a1)
+let b2 = '100';
+console.log(typeof b2)
+let c3 = true;
+console.log(typeof c3)
+
